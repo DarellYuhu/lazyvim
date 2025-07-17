81f606b249ee1b7af8031eb020fcabf58e8c3746
DarellYuhu <darell.mona242@gmail.com>
2025-07-17 18:48:42 +0700

    add ai things

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("i", "ii", "<Esc>", { noremap = true })
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true })
+vim.keymap.set("i", "JK", "<Esc>", { noremap = true })
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true })
